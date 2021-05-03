d22ef0e4cf2946e5c567be0cdd6c2ee43cd0eac9
sf-minsk <sf-minsk@outlook.com>
2021-05-03 21:13:21 +0300

    HW5 GH-PAGES complete

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
+import s from './Header.module.css'
+import menuImage from "./pages/menu.png"
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.menu}>
+            <div className={s.navBlock}>
+                <NavLink to='/pre-junior'>Junior-</NavLink>
+                <NavLink to='/junior'>Junior</NavLink>
+                <NavLink to='/junior-plus'>Junior+</NavLink>
+            </div>
+            <div className={s.imgBlock}>
+                <img src={menuImage}/>
+            </div>
         </div>
     )
 }
